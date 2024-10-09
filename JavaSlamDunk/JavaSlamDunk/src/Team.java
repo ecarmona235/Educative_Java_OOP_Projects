1569fdf9f16e31e15e78479ea28333817c1be700
ecarmona235 <ecarmona235@gmail.com>
2024-10-09 15:43:14 -0700

    Started team class needs to be finished.

--- JavaSlamDunk/JavaSlamDunk/src/Team.java
+++ JavaSlamDunk/JavaSlamDunk/src/Team.java
@@ -1,3 +1,34 @@
+import java.util.List;
+
+
 public class Team {
+    private String name, conference;
+    private static List<Player> teamList;
+
+    public Team(String name, String conference){
+        this.name = name;
+        this.conference = conference;
+    }
     
+
+    public String getName(){
+        return this.name;
+    }
+    public String getConference(){
+        return this.conference;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setConference(String conference){
+        this.conference = conference;
+    }
+    public static List<Player> populateTeams(String[][] playerList){
+        teamList = Player.populatePlayer(playerList);
+        return teamList;
+    }
+
+
+    
+
 }
